bf169a82a15a8d1e97eee075b5bad53e6c288449
L-Mosca <88050952+L-Mosca@users.noreply.github.com>
2021-08-18 21:10:09 -0300

    UX design 0.1

--- app/src/main/java/br/com/monitorwise/Dashboard/DashboardActivity.java
+++ app/src/main/java/br/com/monitorwise/Dashboard/DashboardActivity.java
@@ -1,19 +1,15 @@
 package br.com.monitorwise.Dashboard;
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import br.com.monitorwise.Fragments.FirstFragment;
+import br.com.monitorwise.Fragments.SecondFragment;
+import br.com.monitorwise.Fragments.ThirdFragment;
 import br.com.monitorwise.R;
 
 public class DashboardActivity extends AppCompatActivity {
@@ -27,63 +23,37 @@ public class DashboardActivity extends AppCompatActivity {
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
 
-        RecyclerView rvCalendar = findViewById(R.id.recycler_view_calendar);
 
-        // Comportamento de exibição da recycler view
-        rvCalendar.setLayoutManager(new LinearLayoutManager(this));
-
-        CalendarAdapter calendarAdapter = new CalendarAdapter();    // Instancia o adaptador
-        rvCalendar.setAdapter(calendarAdapter);                     // Define o adaptador
-
-
+        // Bottom Navigation Menu test
         bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
 
            switch(item.getItemId()){
                case R.id.calendarFragment:
+                   ft.replace(R.id.fragment_content, new FirstFragment());
+                   ft.commit();
+                   Toast.makeText(this, "Exibe as informações do monitor", Toast.LENGTH_SHORT).show();
                    Toast.makeText(this, "Exibe o calendario", Toast.LENGTH_SHORT).show();
                    break;
 
                case R.id.monitorFragment:
+                   ft = fm.beginTransaction();
+                   ft.replace(R.id.fragment_content, new SecondFragment());
+                   ft.commit();
                    Toast.makeText(this, "Exibe as informações do monitor", Toast.LENGTH_SHORT).show();
                    break;
 
                case R.id.historicFragment:
-                   Toast.makeText(this, "Exibe o histórico", Toast.LENGTH_SHORT).show();
+                   ft = fm.beginTransaction();
+                   ft.replace(R.id.fragment_content, new ThirdFragment());
+                   ft.commit();
+
            }
             return true;
         });
 
     }
-
-
-    private class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>{
-
-        @NonNull
-        @Override
-        public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return new CalendarViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull DashboardActivity.CalendarViewHolder holder, int position) {
-
-        }
-
-        // Quantidade de células na recycler view
-        @Override
-        public int getItemCount() {
-            return 20;
-        }
-    }
-
-    // View da celula que esta dentro da recycler view
-    private class CalendarViewHolder extends RecyclerView.ViewHolder{
-
-        public CalendarViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
-    }
-
-
 
 }
