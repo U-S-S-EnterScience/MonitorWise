2aba0aa67eb984b496c2946d996a0c4d97cbe940
L-Mosca <88050952+L-Mosca@users.noreply.github.com>
2021-09-24 19:45:28 -0300

    Apaguei tudo

--- app/src/main/java/br/com/monitorwise/MainActivity.java
+++ app/src/main/java/br/com/monitorwise/MainActivity.java
@@ -21,31 +21,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btnRegisterScreen = findViewById(R.id.button_sign_up);
-        Button btnLogin = findViewById(R.id.button_login);
-        EditText edtPassword = findViewById(R.id.edit_text_password);
-        EditText edtEmail = findViewById(R.id.edit_text_login);
-        CheckBox cbShowPassword = findViewById(R.id.check_box_show_password_login);
-
-        // Quando clica na check box mostra e esconde a senha
-        cbShowPassword.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if(isChecked){
-                edtPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-            }else{
-                edtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
-            }
-        });
-
-
-        // Quando clicar no botao de login -> valida o usuario, esconde o teclado e redireciona o usuário para tela principal
-        btnLogin.setOnClickListener(v -> {
-            hideKeyboardFrom(MainActivity.this, btnLogin);
-            startActivity(new Intent(MainActivity.this, DashboardActivity.class));
-        });
-
-
-        // Quando clicar no btnRegisterScreen -> direciona o usuario para tela de cadastro
-        btnRegisterScreen.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
     }
 }
